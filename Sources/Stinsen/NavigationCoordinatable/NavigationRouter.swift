258577d012ef5f76e7596e45d7262683c461b39e
Mario Baumgartner <mario.baumgartner@bluesource.at>
2021-06-07 10:50:35 +0200

    - added popToRoot function in navigationRouter

--- Sources/Stinsen/NavigationCoordinatable/NavigationRouter.swift
+++ Sources/Stinsen/NavigationCoordinatable/NavigationRouter.swift
@@ -4,6 +4,7 @@ import Resolver
 public class NavigationRouter<T: NavigationRoute>: ObservableObject {
     private let routable: NavigationRoutable
     var root: AnyCoordinatable?
+    var _popToRoot: () -> Void
     public let id: Int?
     
     public func route(to route: T) {
@@ -14,6 +15,10 @@ public class NavigationRouter<T: NavigationRoute>: ObservableObject {
         routable.pop()
     }
     
+    public func popToRoot() {
+        _popToRoot()
+    }
+    
     public func dismiss(onFinished: @escaping (() -> Void) = {}) {
         routable.dismiss(withRootCoordinator: root!, onFinished: onFinished)
     }
@@ -21,6 +26,11 @@ public class NavigationRouter<T: NavigationRoute>: ObservableObject {
     init<U: NavigationCoordinatable>(id: Int?, coordinator: U) {
         self.id = id
         self.routable = NavigationRoutable(coordinator: coordinator)
+        
+        _popToRoot = {
+            coordinator.navigationStack.popTo(-1)
+        }
+        
         Resolver.main.register{ self }
     }
 }
